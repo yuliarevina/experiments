4c3557bb111c290f666acb1c68c7b750c5576fb6
Yulia Revina <yrevina@ntu.edu.sg>
2017-12-14 17:10:07 +0800

    Show Fix
    For MRI the cross is size 40.

--- AlternativeShowFixRed.m
+++ AlternativeShowFixRed.m
@@ -1,9 +1,9 @@
 if strcmp(bs_eye,'right')
-    Screen('TextSize', window, 30);
+    Screen('TextSize', window, 40); %40 for MRI, 20 is fine for behavioural
 %     Screen('DrawFormattedText',window, '+', r_fix_cord1(1), yCenter,white);
     [nx, ny] = DrawFormattedText(window, '+', r_fix_cord1(1), 'center', [1 0 0]);
 else %left eye BS, fix on RIGHT side
-    Screen('TextSize', window, 30);
+    Screen('TextSize', window, 40);
     [nx, ny] = DrawFormattedText(window, '+', l_fix_cord1(1), 'center', [1 0 0]);
 %     Screen('DrawFormattedText',window, '+', l_fix_cord1(1), yCenter,white);
 end
