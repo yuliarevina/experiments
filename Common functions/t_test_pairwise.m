1401a2752a102a0dbd184381d1a7d71955de0c23
Yulia Revina <yrevina@ntu.edu.sg>
2020-09-22 15:25:58 +0800

    Update t_test_pairwise.m
    T-test function now outputs the means of the datasets.

--- Common functions/t_test_pairwise.m
+++ Common functions/t_test_pairwise.m
@@ -1,9 +1,9 @@
-function [H,P, STATS, raweffect, SE, corrected_SE] = t_test_pairwise (data1, data2)
+function [mean1, mean2, H,P, STATS, raweffect, SE, corrected_SE] = t_test_pairwise (data1, data2)
 
-nanmean(data1)
-nanmean(data2)
-[H,P, ~,STATS] = ttest(data1, data2)
+mean1 = nanmean(data1);
+mean2 = nanmean(data2);
+[H,P, ~,STATS] = ttest(data1, data2);
 %for bayes
-raweffect = nanmean(data1) - nanmean(data2)
-SE = raweffect/STATS.tstat
-corrected_SE = SE*(1 + 20/(STATS.df*STATS.df)) %for small sample sizes <30
+raweffect = nanmean(data1) - nanmean(data2);
+SE = raweffect/STATS.tstat;
+corrected_SE = SE*(1 + 20/(STATS.df*STATS.df)); %for small sample sizes <30
